d3b3e3785c2d7413f54fb85cb8193843596f6f78
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2021-12-09 05:24:10 +0530

    Using space-indentation.

--- demo-study/src/main/java/com/example/study/demostudy/DemoStudyApplication.java
+++ demo-study/src/main/java/com/example/study/demostudy/DemoStudyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoStudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoStudyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStudyApplication.class, args);
+    }
 
 }
